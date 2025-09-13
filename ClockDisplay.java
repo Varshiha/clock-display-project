be044d4b0d086d83754285ce91f8ef0efec48365
Varshiha <varshihaa@icloud.com>
2025-09-12 23:40:32 -0400

    Questions 51 and 52 for Part 3. Anwser for question 52 is in the readme file of the part 3 repository which is midi-player project

--- ClockDisplay.java
+++ ClockDisplay.java
@@ -26,9 +26,19 @@ public class ClockDisplay
      */
     public ClockDisplay()
     {
+        long millis = System.currentTimeMillis();
+        long seconds = millis / 1000;
+        int currentMinutes = (int) (seconds / 60 % 60);
+        int currentHours = (int) (seconds / (60 * 60) % 24);
+        
         hours = new NumberDisplay(24);
         minutes = new NumberDisplay(60);
+        
+        hours.setValue(currentHours);
+        minutes.setValue(currentMinutes);
+        
         updateDisplay();
+        
     }
 
     /**
